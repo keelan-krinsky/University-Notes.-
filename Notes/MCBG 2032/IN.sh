0aa39d40385d73721c72d6ad4b91e0f0ddf58e27
Keelan-Krinsky <keelankrinsky@gmail.com>
2018-02-18 07:25:15 +0200

    reorganised notes

--- Notes/MCBG 2032/IN.sh
+++ Notes/MCBG 2032/IN.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pandoc  -f markdown -t latex  'Integrated Notes.md' -s -o  'Integrated Notes.tex'
+pandoc  -f markdown -t latex  'Integrated Notes.markdown' -s -o  'Integrated Notes.tex'
 pdflatex 'Integrated Notes.tex'
